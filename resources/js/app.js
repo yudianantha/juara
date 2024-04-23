16b1c48d26a795479abbc04fb13617172e06897a
Yudi Anantha <ananthayudi@yahoo.com>
2024-04-23 22:24:10 +0800

    verce

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+const chrome = require('@sparticuz/chromium');
+const puppeteer = require('puppeteer-core');
+const production = process.env.NODE_ENV === 'production';
+
+const getPage = async () => {
+    const browser = await puppeteer.launch(
+        production ? {
+            args: chrome.args,
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath(),
+            headless: 'new',
+            ignoreHTTPSErrors: true
+        } : {
+            headless: 'new',
+            executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        }
+    );
+    const page = await browser.newPage();
+    return page;
+};
